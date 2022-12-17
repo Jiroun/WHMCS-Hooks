83101f301af5b22741c46ac7cb0257762cb0ad77
Aladdin Jiroun <66474560+Jiroun@users.noreply.github.com>
2022-12-18 02:38:39 +0400

    Update HidePaymentGateWaysIfTotalCartIsZero.php

--- HidePaymentGateWaysIfTotalCartIsZero.php
+++ HidePaymentGateWaysIfTotalCartIsZero.php
@@ -5,10 +5,30 @@
  * @author      Aladdin Jiroun
  */
 
-
 if (!defined("WHMCS"))
     die("This file cannot be accessed directly");
 
+
+// IF you Want to Select PayPal TO hide Credit Card InputFields USE THIS HOOK
+add_hook('ClientAreaPageCart', 1, function($vars) {
+    
+    if ($vars['rawtotal'] == 0.00)
+    {
+        add_hook('ClientAreaFooterOutput', 1, function($vars) {
+        $v = "input[value='paypal']";
+        $return = '
+<script>
+    document.querySelector("'.$v.'").checked = true;
+    const PGC = document.getElementById("paymentGatewaysContainer");
+    PGC.style.display = "none";
+    ';
+        return $return;
+        }); 
+    }
+});
+
+
+// OR USE THIS WILL HIDE Credit Card InputFields AFTER 1 SECOND 
 add_hook('ClientAreaPageCart', 1, function($vars) {
     
     if ($vars['rawtotal'] == 0.00)
