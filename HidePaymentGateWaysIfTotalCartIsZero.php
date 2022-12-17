6c499cd5c5e0608d7252846acec797949849ceb8
Aladdin Jiroun <66474560+Jiroun@users.noreply.github.com>
2022-12-18 00:14:39 +0400

    Create HidePaymentGateWaysIfTotalCartIsZero.php

---
+++ HidePaymentGateWaysIfTotalCartIsZero.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Hide paymentGateways And credit Card InputFields When Total at Cart Checkout is Zero
+ * @package     WHMCS
+ * @author      Aladdin Jiroun
+ */
+
+
+if (!defined("WHMCS"))
+    die("This file cannot be accessed directly");
+    
+    
+
+add_hook('ClientAreaPageCart', 1, function($vars) {
+    
+    if ($vars['rawtotal'] == 0.00)
+    
+    add_hook('ClientAreaFooterOutput', 1, function($vars) {
+
+        $return = '
+    <script>
+const PGC = document.getElementById("paymentGatewaysContainer");
+const CCIF = document.getElementById("creditCardInputFields");
+PGC.style.display = "none";
+CCIF.style.display = "none";
+</script>
+    ';
+    
+    return $return;
+});
+
+});
